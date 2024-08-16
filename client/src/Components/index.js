13397dc0f206c7b58d1b46b0b0240efc553811d2
Bobby Tripathi <80473945+Bobby278tr@users.noreply.github.com>
2024-08-17 01:05:06 +0530

    Setting up the homepage and adding the components required

---
+++ client/src/Components/index.js
@@ -0,0 +1,10 @@
+import Card from "./Card";
+import FormField from "./FormField";
+import Loader from "./Loader";
+
+export {
+  Card,
+  FormField,
+  Loader,
+  // Add more components here if needed
+};
